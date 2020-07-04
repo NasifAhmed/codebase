16e972bdb735ecd08b609d4c38c9d502974624d9
Nasif Ahmed <nasif2ahmed@gmail.com>
2020-07-04 23:21:46 +0600

    Started automate the boring stuff with python course on Udemy

---
+++ Codes/Python/Automate The Boring Stuff/test.py
@@ -0,0 +1,13 @@
+#Here start my python programming
+
+print('Hellow World')
+
+print('What is your name ?') 
+name = input()
+
+print ('Your name is ' + name) 
+
+age = input()
+
+print('Your age is 10 times of ' + str(float(float(age)/10)))
+
