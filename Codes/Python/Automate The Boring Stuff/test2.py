36fbff1d8519ff8fec252deca108c585480858ac
Nasif Ahmed <nasif2ahmed@gmail.com>
2020-07-13 21:52:34 +0600

    ..

---
+++ Codes/Python/Automate The Boring Stuff/test2.py
@@ -0,0 +1,12 @@
+name = 'Moga'
+if name == 'Moga':
+    print('Hi Alice')
+else:
+    print('bye Alice')
+
+for i in range(10):
+    print(i)
+for i in range(34,88):
+    print(i)
+for i in range(34,88,10):
+    print(i)
